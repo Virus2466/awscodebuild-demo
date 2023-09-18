fd355630e5cc581eb6b986c6dc25ec6c14a16338
Dev Patel <49341610+Virus2466@users.noreply.github.com>
2023-09-18 23:38:02 +0530

    edited scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e 
 
 #pull the docker image 
-docker push virus2466/flask-demo-aws:latest
+docker push virus2466/flask-demo-aws
 
 # run the image 
 docker run -d -p 5000:5000 virus2466/flask-demo-aws:tagname
